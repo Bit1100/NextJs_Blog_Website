d691cc1b8b8ca27afeec54553a2c6c3a7511ad98
Bit1100 <surajkeshari1100@gmail.com>
2023-02-15 04:27:32 -0800

    Switch to fallback false

--- components/Articles/Article.js
+++ components/Articles/Article.js
@@ -1,21 +1,14 @@
-import { useRouter } from "next/router";
 import { Card, Item1, Item2, Item3, BackButton } from "./Article.styles";
 import { setTitleCase } from "../../helpers";
 
 const Article = ({ article: blog }) => {
-  const router = useRouter();
-
-  if (router.isFallback) {
-    return <h2>Loading..</h2>;
-  }
-
   return (
     <Card article>
       <Item1>Cat: {setTitleCase(blog?.category)}</Item1>
       <Item2 noClamp> {blog?.title}</Item2>
       <Item3 noClamp>{blog?.body}</Item3>
       <BackButton variant="outlined" onClick={() => window.history.back()}>
-        More Articles..
+        Go Back
       </BackButton>
     </Card>
   );
