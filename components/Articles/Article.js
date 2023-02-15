159969774ef7463299274313367e6006c1c4e0d4
Bit1100 <surajkeshari1100@gmail.com>
2023-02-15 02:17:17 -0800

    Fix GoBack() Bug

--- components/Articles/Article.js
+++ components/Articles/Article.js
@@ -4,7 +4,7 @@ import { setTitleCase } from "../../helpers";
 
 const Article = ({ article }) => {
   const router = useRouter();
-  const { id, title, body, category } = article;
+  const { title, body, category } = article;
 
   if (router.isFallback) {
     return <h2>Loading..</h2>;
@@ -15,7 +15,7 @@ const Article = ({ article }) => {
       <Item1>Cat: {setTitleCase(category)}</Item1>
       <Item2 noClamp> {title}</Item2>
       <Item3 noClamp>{body}</Item3>
-      <BackButton variant="outlined" onClick={() => router.push("/articles")}>
+      <BackButton variant="outlined" onClick={() => window.history.back()}>
         More Articles..
       </BackButton>
     </Card>
