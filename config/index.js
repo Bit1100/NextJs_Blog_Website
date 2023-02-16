eb0d6e7c758a113c0aebb2fb8ebde3036a1f5e62
Bit1100 <surajkeshari1100@gmail.com>
2023-02-16 03:43:04 -0800

    Fix Node_Env Issue

--- config/index.js
+++ config/index.js
@@ -1,7 +1,5 @@
-// const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== "production";
 
-// export const server = dev
-//   ? "http://localhost:3000"
-//   : "https://bit1100.github.io/Basic_Blog_NextJs_WebSite";
-
-export const server = "http://localhost:3000";
+export const server = dev
+  ? "http://localhost:3000/"
+  : "https://next-js-blog-website-1.vercel.app/";
